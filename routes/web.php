5ac64173ea2c018e10e96687ef83e02996bf3c5f
Théo Lebiez <123150037+Deeffault@users.noreply.github.com>
2026-07-31 22:25:58 +0200

    Add test_api route returning a string

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,9 @@ Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('test_api', function () {
+    return 'mgl805';
+})->name('test_api');
+
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
